53646b4e13fa3188e98b9e45bd3e8807e716eefe
Wendy2498 <wfaulk2498@smail.pcd.edu>
2021-06-08 03:00:19 -0700

    #2 Added part of function shell/outline to this version.

--- FutureValue/frmFutureValue.cs
+++ FutureValue/frmFutureValue.cs
@@ -29,41 +29,45 @@ namespace FutureValue
             decimal monthlyInvestment = 0;
 
             try
-            {
-                monthlyInvestment = Convert.ToDecimal(txtMonthlyInvestment.Text);
-                yearlyInterestRate = Convert.ToDecimal(txtInterestRate.Text);
-                years = Convert.ToInt32(txtYears.Text);
-                int months = years * 12;
-                decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
-                decimal futureValue = this.CalculateFutureValue(
-                    monthlyInvestment, monthlyInterestRate, months);
-                txtFutureValue.Text = futureValue.ToString("c");
-                txtMonthlyInvestment.Focus();
+            {  
+                if (isValidData())
+                {
+                    monthlyInvestment = Convert.ToDecimal(txtMonthlyInvestment.Text);
+                    yearlyInterestRate = Convert.ToDecimal(txtInterestRate.Text);
+                    years = Convert.ToInt32(txtYears.Text);
+                }
             }
-            //catch (formatexception)  //a specific exception
-            //{       //this error's message box w/ error type
-            //    messagebox.show("a format exception has occured. please check your entries", "entry error");
-            //}
+            catch (FormatException)  //a specific exception
+            {       //this error's message box w/ error type
+                MessageBox.Show("a format exception has occured. please check your entries", "entry error");
+            }
 
-            //catch (overflowexception)   //another specific exception
-            //{       //this error's message box w/ error type
-            //    messagebox.show("an overflow exception has occured. please enter smaller values", "overflow error");
-            //}
-            catch (Exception ex)
+            catch (OverflowException)   //another specific exception
+            {       //this error's message box w/ error type
+                MessageBox.Show("an overflow exception has occured. please enter smaller values", "overflow error");
+            }
+            catch (Exception ex)    //other exceptions
             {
                 MessageBox.Show(ex.Message + ex.GetType().ToString() + ex.StackTrace, "Error");
             }
+            int months = years * 12;
+            decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
+            decimal futureValue = this.CalculateFutureValue(
+                monthlyInvestment, monthlyInterestRate, months);
+            txtFutureValue.Text = futureValue.ToString("c");
+            txtMonthlyInvestment.Focus();
+
         }
 
         private decimal CalculateFutureValue(decimal monthlyInvestment,
             decimal monthlyInterestRate, int months)
         {
             decimal futureValue = 0m;
-            //create throw statement that throws a new exception of the Exception class regardless of the results of the calculation//
-            if (monthlyInvestment >= 0)
-            {   //Indicate unknown error
-                throw new Exception("Unknown Error.");
-            }
+            ////create throw statement that throws a new exception of the Exception class regardless of the results of the calculation//
+            //if (monthlyInvestment >= 0)
+            //{   //Indicate unknown error
+            //    throw new Exception("Unknown Error.");
+            //}
             for (int i = 0; i < months; i++)
             {
                 futureValue = (futureValue + monthlyInvestment)
@@ -71,6 +75,58 @@ namespace FutureValue
             }
             return futureValue;
         }
+
+        public bool isValidData()
+        {
+            return
+                isPresent(txtMonthlyInvestment, "Monthly investment") &&
+                isPresent(txtInterestRate, "Yearly interest rate") &&
+                isPresent(txtYears, "Number of years");
+        }
+
+        public bool isPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        public bool isDecimal(string value, string name)
+        {
+            /*fill in code from textbook*/
+            string msg = "";
+            if (!Decimal.TryParse(value, out _))
+            {
+                msg += name + " must be a vaild decimal value.";
+                return false;
+            }
+            return true;
+        }
+
+        //public bool IsInt32(TextBox textBox, string name)
+        //{
+        //    /*fill in code from textbook*/
+
+
+
+        //    return true;
+        //}
+
+        //public bool (TextBox textBox, string name)
+        //{
+        //    /*fill in code from textbook*/
+        //    if ))
+        //    {
+        //        MessageBox.Show(name + " must be a valid decimal value.");
+        //        textBox.Focus();
+        //        return false;
+        //    }
+        //    return true;
+        //}
 
         private void btnExit_Click(object sender, EventArgs e)
         {
